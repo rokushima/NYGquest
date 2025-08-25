d7a23506525c54b4f896d1c3d8e2e20947bbe7d4
rokushima <105997557+rokushima@users.noreply.github.com>
2025-08-26 07:23:43 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ function valueChange(){
     if(element.value=="place1"){
         $('.quiz-img').attr("src","images/image1.png");
         $('.quiz-txt').text("");
-        $('.hint1').append("<label><input type='checkbox' id='check1' name='check'/>ヒント1</label>")
         check1 = document.getElementById("check1");
         check1.addEventListener('change', valueChange);
         if (check1.checked){
@@ -11,6 +10,7 @@ function valueChange(){
             check1 = document.getElementById("check2");
             check2.addEventListener('change', valueChange);
         }else{
+            $('.hint1').append("<label><input type='checkbox' id='check1' name='check'/>ヒント1</label>")
             $('.hint1-img').attr("src","");
             $('.check2').remove();
         }
