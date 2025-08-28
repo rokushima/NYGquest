239f4ce49ca8acfbf0434c0608cea41b4c2a7536
rokushima <105997557+rokushima@users.noreply.github.com>
2025-08-29 07:18:23 +0900

    Update main.js

--- main.js
+++ main.js
@@ -236,7 +236,7 @@ $(document).ready(function(){
     img.src = "https://rokushima.github.io/NYGquest/images/image11_1.svg";
     img.src = "https://rokushima.github.io/NYGquest/images/image11_2.svg";
     img.src = "https://rokushima.github.io/NYGquest/images/image12.svg";
-    img.src = "https://rokushima.github.io/NYGquest/images/image12_1.svg";
+    /*img.src = "https://rokushima.github.io/NYGquest/images/image12_1.svg";
     img.src = "https://rokushima.github.io/NYGquest/images/image12_2.svg";
     img.src = "https://rokushima.github.io/NYGquest/images/image13.svg";
     img.src = "https://rokushima.github.io/NYGquest/images/image14.svg";
@@ -246,7 +246,7 @@ $(document).ready(function(){
     img.src = "https://rokushima.github.io/NYGquest/images/image15_1.svg";
     img.src = "https://rokushima.github.io/NYGquest/images/image16.svg";
     img.src = "https://rokushima.github.io/NYGquest/images/image17.svg";
-    
+    */
     let element = document.getElementById('place');
     element.addEventListener('change', valueChange);
     let check1 = document.getElementById("check1");
