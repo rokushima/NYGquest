d48d65edb92093e0db360c3a4b01a7808c9db56f
rokushima <105997557+rokushima@users.noreply.github.com>
2025-08-29 07:01:20 +0900

    Update main.js

--- main.js
+++ main.js
@@ -220,9 +220,17 @@ function hint2(){
 }
 $(document).ready(function(){
     const img = new Image();
-    img.src = "https://rokushima.github.io/NYGquest/images/image6.png"
-    img.src = "https://rokushima.github.io/NYGquest/images/image8.png"
-    img.src = "https://rokushima.github.io/NYGquest/images/image1.svg"
+
+    img.src = "https://rokushima.github.io/NYGquest/images/image1.svg";
+    img.src = "https://rokushima.github.io/NYGquest/images/image1_1.svg";
+    img.src = "https://rokushima.github.io/NYGquest/images/image1_2.svg";
+    img.src = "https://rokushima.github.io/NYGquest/images/image3.svg";
+    img.src = "https://rokushima.github.io/NYGquest/images/image3_1.svg";
+    img.src = "https://rokushima.github.io/NYGquest/images/image4.svg";
+    img.src = "https://rokushima.github.io/NYGquest/images/image5.svg";
+    img.src = "https://rokushima.github.io/NYGquest/images/image6.png";
+    img.src = "https://rokushima.github.io/NYGquest/images/image7.svg";
+    img.src = "https://rokushima.github.io/NYGquest/images/image8.png";
     let element = document.getElementById('place');
     element.addEventListener('change', valueChange);
     let check1 = document.getElementById("check1");
